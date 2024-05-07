98864fa67043859c62f41dd0ae9361959d8b00c8
Kayke-Ti <kaykeloiola@gmail.com>
2024-05-07 13:39:41 -0300

    feat: new component in pages

---
+++ src/components/titleComponent.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+interface Props {
+  title: string;
+}
+
+const TitleComponent: React.FC<Props> = ({ title }) => {
+  return (
+    <span className="text-primary text-3xl md:text-2xl font-semibold flex items-center text-zinc-200 max-[600px]:pl-0 max-[600px]:mb-2 text-[1.6rem] max-[600px] pl-3">
+      <span className="w-6 h-1 bg-blue-600 mr-3 md:mr-4"></span>
+      {title}
+    </span>
+  );
+};
+
+export default TitleComponent;
