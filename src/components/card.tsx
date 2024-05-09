161f356a1d296c67338651f39c7679cdf6e893c2
kaykeeb3 <kaykeloiola@gmail.com>
2024-05-09 15:56:37 -0300

    feat: create new styles in icon

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -12,7 +12,9 @@ const ServiceCard: React.FC<ServiceCardProps> = ({
 }) => {
   return (
     <div className="bg-blue-800/10 w-full xl:h-[45vh] max-[600px]:h-[62vh] md:h-[60vh] md:p-4 p-5 rounded-md">
-      {Icon && <Icon className="w-8 h-8 text-zinc-200 text-left" />}{" "}
+      {Icon && (
+        <Icon className="w-9 h-9 text-zinc-200 text-left rounded-full p-2 bg-blue-950 border border-zinc-200/10" />
+      )}{" "}
       <div className="flex flex-col p-2">
         <h3 className="font-semibold text-2xl text-zinc-200 mb-8">{title}</h3>
         <div>
