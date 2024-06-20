a00e270b76a67fea488ac72957a935ea1073fbcd
kaykeeb3 <kaykeloioladev@gmail.com>
2024-06-20 15:08:41 -0300

    feat: config layout in mobile

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import CustomLink from "./link";
+import CustomLink from "@/components/link";
 import { ArrowLeft } from "lucide-react";
 import { Link, useLocation } from "react-router-dom";
 import Logo from "@/assets/logo.svg";
@@ -15,10 +15,11 @@ function Header() {
       className="flex items-center justify-between py-12 md:pr-5 max-[600px]:p-5"
     >
       <div className="float-start flex justify-start p-2">
-        <Link to="/" className="w-full mx-1 hover:underline">
-          <img src={Logo} alt="Logo da Página" className="w-36" />
-        </Link>
-        {location.pathname !== "/" && (
+        {location.pathname === "/" ? (
+          <Link to="/" className="w-full mx-1 hover:underline max-sm:hidden">
+            <img src={Logo} alt="Logo da Página" className="w-36" />
+          </Link>
+        ) : (
           <Link to="/">
             <ArrowLeft className="w-5 h-5 text-zinc-300 hover:text-zinc-400" />
           </Link>
